036553461650ad154da05e0fd8181d99b7a55819
PierreVieira <pierrevieiraggg@gmail.com>
2025-12-14 23:10:42 -0300

    chore: Remove unused dependencies and imports
    Unused dependencies such as JUnit, Espresso, and AppCompat have been removed from the `libs.versions.toml` file to clean up the project's dependencies. Additionally, an unused import for `NavGraphBuilder` has been removed from `AppNavHost.kt`.

--- core/navigation/src/commonMain/kotlin/com/quare/bibleplanner/core/navigation/AppNavHost.kt
+++ core/navigation/src/commonMain/kotlin/com/quare/bibleplanner/core/navigation/AppNavHost.kt
@@ -1,7 +1,6 @@
 package com.quare.bibleplanner.core.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.quare.bibleplanner.core.model.route.ReadingPlanNavRoute
