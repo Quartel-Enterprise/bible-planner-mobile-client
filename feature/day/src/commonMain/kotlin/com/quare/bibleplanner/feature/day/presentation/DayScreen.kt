b5f086a64bcc3c0e2bb4eda6dfc7ac1bb534cab1
PierreVieira <pierrevieiraggg@gmail.com>
2025-12-16 00:44:27 -0300

    refactor: Extract DayScreen TopAppBar into a component
    The `TopAppBar` from `DayScreen` has been extracted into a new, reusable `DayScreenTopBarComponent`. This refactoring improves the organization and readability of the `DayScreen` composable by separating the top bar's logic and UI into its own dedicated component.

--- feature/day/src/commonMain/kotlin/com/quare/bibleplanner/feature/day/presentation/DayScreen.kt
+++ feature/day/src/commonMain/kotlin/com/quare/bibleplanner/feature/day/presentation/DayScreen.kt
@@ -4,31 +4,19 @@ import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
-import bibleplanner.feature.day.generated.resources.Res
-import bibleplanner.feature.day.generated.resources.day_title_part
-import bibleplanner.feature.day.generated.resources.week_title_part
-import com.quare.bibleplanner.core.utils.SharedTransitionAnimationUtils
-import com.quare.bibleplanner.feature.day.presentation.component.DayProgress
+import com.quare.bibleplanner.feature.day.presentation.component.DayScreenTopBarComponent
 import com.quare.bibleplanner.feature.day.presentation.content.DayContent
 import com.quare.bibleplanner.feature.day.presentation.model.DayUiEvent
 import com.quare.bibleplanner.feature.day.presentation.model.DayUiState
-import com.quare.bibleplanner.ui.component.icon.BackIcon
-import com.quare.bibleplanner.ui.component.spacer.VerticalSpacer
-import org.jetbrains.compose.resources.stringResource
 
 private const val MAX_CONTENT_WIDTH = 600
 
@@ -46,68 +34,12 @@ internal fun DayScreen(
             .fillMaxSize()
             .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            TopAppBar(
-                title = {
-                    (uiState as? DayUiState.Loaded)?.run {
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                        ) {
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                            ) {
-                                with(sharedTransitionScope) {
-                                    Text(
-                                        text = stringResource(
-                                            Res.string.day_title_part,
-                                            day.number,
-                                        ),
-                                        modifier = Modifier.sharedElement(
-                                            sharedContentState = rememberSharedContentState(
-                                                key = SharedTransitionAnimationUtils.buildDayNumberId(
-                                                    weekNumber = weekNumber,
-                                                    dayNumebr = day.number,
-                                                ),
-                                            ),
-                                            animatedVisibilityScope = animatedContentScope,
-                                        ),
-                                    )
-                                    Text(
-                                        modifier = Modifier.sharedElement(
-                                            sharedContentState = rememberSharedContentState(
-                                                key = SharedTransitionAnimationUtils.buildWeekSeparatorId(weekNumber),
-                                            ),
-                                            animatedVisibilityScope = animatedContentScope,
-                                        ),
-                                        text = " — ",
-                                    )
-                                    Text(
-                                        text = stringResource(
-                                            Res.string.week_title_part,
-                                            weekNumber,
-                                        ),
-                                        modifier = Modifier.sharedElement(
-                                            sharedContentState = rememberSharedContentState(
-                                                key = SharedTransitionAnimationUtils.buildWeekNumberId(
-                                                    weekNumber = weekNumber,
-                                                ),
-                                            ),
-                                            animatedVisibilityScope = animatedContentScope,
-                                        ),
-                                    )
-                                }
-                            }
-                            VerticalSpacer(4)
-                            DayProgress(
-                                passages = day.passages,
-                                books = books,
-                            )
-                        }
-                    }
-                },
-                navigationIcon = {
-                    BackIcon(onBackClick = { onEvent(DayUiEvent.OnBackClick) })
-                },
+            DayScreenTopBarComponent(
+                uiState = uiState,
+                sharedTransitionScope = sharedTransitionScope,
+                animatedContentScope = animatedContentScope,
                 scrollBehavior = scrollBehavior,
+                onEvent = onEvent
             )
         },
     ) { paddingValues ->
---
+++ feature/day/src/commonMain/kotlin/com/quare/bibleplanner/feature/day/presentation/component/DayScreenTopBarComponent.kt
@@ -0,0 +1,97 @@
+package com.quare.bibleplanner.feature.day.presentation.component
+
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import bibleplanner.feature.day.generated.resources.Res
+import bibleplanner.feature.day.generated.resources.day_title_part
+import bibleplanner.feature.day.generated.resources.week_title_part
+import com.quare.bibleplanner.core.utils.SharedTransitionAnimationUtils
+import com.quare.bibleplanner.feature.day.presentation.model.DayUiEvent
+import com.quare.bibleplanner.feature.day.presentation.model.DayUiState
+import com.quare.bibleplanner.ui.component.icon.BackIcon
+import com.quare.bibleplanner.ui.component.spacer.VerticalSpacer
+import org.jetbrains.compose.resources.stringResource
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalSharedTransitionApi::class)
+@Composable
+internal fun DayScreenTopBarComponent(
+    uiState: DayUiState,
+    sharedTransitionScope: SharedTransitionScope,
+    animatedContentScope: AnimatedContentScope,
+    scrollBehavior: TopAppBarScrollBehavior,
+    onEvent: (DayUiEvent) -> Unit,
+) {
+    TopAppBar(
+        title = {
+            (uiState as? DayUiState.Loaded)?.run {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        with(sharedTransitionScope) {
+                            Text(
+                                text = stringResource(
+                                    Res.string.day_title_part,
+                                    day.number,
+                                ),
+                                modifier = Modifier.sharedElement(
+                                    sharedContentState = rememberSharedContentState(
+                                        key = SharedTransitionAnimationUtils.buildDayNumberId(
+                                            weekNumber = weekNumber,
+                                            dayNumebr = day.number,
+                                        ),
+                                    ),
+                                    animatedVisibilityScope = animatedContentScope,
+                                ),
+                            )
+                            Text(
+                                modifier = Modifier.sharedElement(
+                                    sharedContentState = rememberSharedContentState(
+                                        key = SharedTransitionAnimationUtils.buildWeekSeparatorId(weekNumber),
+                                    ),
+                                    animatedVisibilityScope = animatedContentScope,
+                                ),
+                                text = " — ",
+                            )
+                            Text(
+                                text = stringResource(
+                                    Res.string.week_title_part,
+                                    weekNumber,
+                                ),
+                                modifier = Modifier.sharedElement(
+                                    sharedContentState = rememberSharedContentState(
+                                        key = SharedTransitionAnimationUtils.buildWeekNumberId(
+                                            weekNumber = weekNumber,
+                                        ),
+                                    ),
+                                    animatedVisibilityScope = animatedContentScope,
+                                ),
+                            )
+                        }
+                    }
+                    VerticalSpacer(4)
+                    DayProgress(
+                        passages = day.passages,
+                        books = books,
+                    )
+                }
+            }
+        },
+        navigationIcon = {
+            BackIcon(onBackClick = { onEvent(DayUiEvent.OnBackClick) })
+        },
+        scrollBehavior = scrollBehavior,
+    )
+}
