df2ba658186cdb38a779178e6ca59d7b0820a711
PierreVieira <pierrevieiraggg@gmail.com>
2026-01-05 03:42:30 -0300

    chore: update version to 1.6.1 and increment project version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -109,7 +109,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "com.quare.bibleplanner"
-            packageVersion = "1.6.0"
+            packageVersion = "1.6.1"
 
             macOS {
                 iconFile.set(project.file("../icons/bible_planner_logo.icns"))
