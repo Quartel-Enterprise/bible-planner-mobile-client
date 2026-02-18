dd38f0a2b246b8a38cd2c3fed29aca3ac604d26b
PierreVieira <pierrevieiraggg@gmail.com>
2026-02-17 21:27:28 -0300

    refactor: Optimize EnsureStartDateIsAvailableUseCase for improved flow handling

--- composeApp/src/commonMain/kotlin/com/quare/bibleplanner/domain/usecase/impl/EnsureStartDateIsAvailableUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/quare/bibleplanner/domain/usecase/impl/EnsureStartDateIsAvailableUseCase.kt
@@ -2,16 +2,13 @@ package com.quare.bibleplanner.domain.usecase.impl
 
 import com.quare.bibleplanner.core.plan.domain.usecase.GetPlanStartDateFlowUseCase
 import com.quare.bibleplanner.core.plan.domain.usecase.SetPlanStartTimeUseCase
+import kotlinx.coroutines.flow.first
 
 internal class EnsureStartDateIsAvailableUseCase(
-    private val getPlanStartDate: GetPlanStartDateFlowUseCase,
+    private val getPlanStartDateFlow: GetPlanStartDateFlowUseCase,
     private val setPlanStartTime: SetPlanStartTimeUseCase,
 ) {
     suspend operator fun invoke() {
-        getPlanStartDate().collect { startDate ->
-            if (startDate == null) {
-                setPlanStartTime(SetPlanStartTimeUseCase.Strategy.Now)
-            }
-        }
+        getPlanStartDateFlow().first() ?: setPlanStartTime(SetPlanStartTimeUseCase.Strategy.Now)
     }
 }
