07f04f357b354b0238f16cb963a2698737985acb
PierreVieira <pierrevieiraggg@gmail.com>
2026-02-20 18:16:44 -0300

    refactor(bible_version): Simplify SetSelectedVersionUseCase
    This commit simplifies the `SetSelectedVersionUseCase` by removing the logic that automatically managed download states (downloading the selected version and pausing others).
    
    The use case is now solely responsible for setting the selected Bible version ID in the repository, leaving download management to other components. This improves separation of concerns.

--- feature/bible_version/src/commonMain/kotlin/com/quare/bibleplanner/feature/bibleversion/domain/usecase/SetSelectedVersionUseCase.kt
+++ feature/bible_version/src/commonMain/kotlin/com/quare/bibleplanner/feature/bibleversion/domain/usecase/SetSelectedVersionUseCase.kt
@@ -1,26 +1,11 @@
 package com.quare.bibleplanner.feature.bibleversion.domain.usecase
 
 import com.quare.bibleplanner.core.books.domain.repository.BibleRepository
-import com.quare.bibleplanner.core.model.downloadstatus.DownloadStatusModel
-import com.quare.bibleplanner.feature.bibleversion.domain.BibleVersionDownloaderFacade
-import kotlinx.coroutines.flow.first
 
 class SetSelectedVersionUseCase(
     private val repository: BibleRepository,
-    private val bibleVersionDownloaderFacade: BibleVersionDownloaderFacade,
 ) {
     suspend operator fun invoke(selectedId: String) {
-        val bibles = repository.getBiblesFlow().first()
-        bibles.forEach { bible ->
-            val versionId = bible.version.id
-            bibleVersionDownloaderFacade.run {
-                if (versionId == selectedId) {
-                    downloadVersion(versionId)
-                } else if (bible.downloadStatus is DownloadStatusModel.InProgress.Downloading) {
-                    pauseDownload(versionId)
-                }
-            }
-        }
         repository.setSelectedVersionId(selectedId)
     }
 }
