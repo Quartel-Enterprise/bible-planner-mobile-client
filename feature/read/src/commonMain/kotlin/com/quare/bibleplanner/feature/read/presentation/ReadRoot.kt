02bb276d80d49b3d512e90670c34d292151de6f1
PierreVieira <pierrevieiraggg@gmail.com>
2026-02-27 14:40:55 -0300

    chore: Upgrade Gradle and library dependencies to latest versions

--- feature/read/src/commonMain/kotlin/com/quare/bibleplanner/feature/read/presentation/ReadRoot.kt
+++ feature/read/src/commonMain/kotlin/com/quare/bibleplanner/feature/read/presentation/ReadRoot.kt
@@ -1,17 +1,13 @@
 package com.quare.bibleplanner.feature.read.presentation
 
 import androidx.compose.animation.SharedTransitionScope
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.quare.bibleplanner.core.model.route.ReadNavRoute
-import com.quare.bibleplanner.feature.read.presentation.model.ReadUiAction
 import com.quare.bibleplanner.feature.read.presentation.utils.ReadUiActionCollector
-import com.quare.bibleplanner.ui.utils.ActionCollector
-import kotlinx.coroutines.flow.Flow
 import org.koin.compose.viewmodel.koinViewModel
 
 fun NavGraphBuilder.read(
