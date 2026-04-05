0f393c0077bb89efd1810c2b1e9566b4e0ab1c45
PierreVieira <pierrevieiraggg@gmail.com>
2026-04-05 00:01:19 -0300

    feat: Implement background task for Bible initialization and add notification handling

---
+++ iosApp/iosApp/NotificationDelegate.swift
@@ -0,0 +1,14 @@
+import UserNotifications
+
+class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
+    static let shared = NotificationDelegate()
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler:
+            @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.banner, .sound])
+    }
+}
