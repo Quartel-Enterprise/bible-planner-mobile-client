4d7f2e6e473ec02279073afc029d6921717b2455
PierreVieira <pierrevieiraggg@gmail.com>
2026-04-06 23:44:15 -0300

    refactor: update `initializeDatabase` function signature to remove default parameter

--- core/books/src/commonMain/kotlin/com/quare/bibleplanner/core/books/domain/repository/BooksRepository.kt
+++ core/books/src/commonMain/kotlin/com/quare/bibleplanner/core/books/domain/repository/BooksRepository.kt
@@ -11,7 +11,7 @@ interface BooksRepository {
 
     suspend fun getBooks(): List<BookDataModel>
 
-    suspend fun initializeDatabase(onProgress: (current: Int, total: Int) -> Unit = { _, _ -> })
+    suspend fun initializeDatabase(onProgress: (current: Int, total: Int) -> Unit)
 
     suspend fun updateBookFavoriteStatus(
         bookId: BookId,
