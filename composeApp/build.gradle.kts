984f1aaaf95c76fc71f7a08ead19aafb065cbfdc
PierreVieira <pierrevieiraggg@gmail.com>
2026-05-10 23:57:47 -0300

    chore: Bump version to 1.12.0 and update project settings

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -120,7 +120,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "com.quare.bibleplanner"
-            packageVersion = "1.11.0"
+            packageVersion = "1.12.0"
 
             macOS {
                 iconFile.set(project.file("../icons/bible_planner_logo.icns"))
