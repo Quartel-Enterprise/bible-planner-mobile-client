4ad63cca9bc2c81e5b8d450e4de7f2adad9ab23f
PierreVieira <pierrevieiraggg@gmail.com>
2026-05-17 22:05:00 -0300

    feat: Enhance notification handling and improve navigation event management

--- composeApp/src/androidMain/kotlin/com/quare/bibleplanner/MainActivityViewModel.kt
+++ composeApp/src/androidMain/kotlin/com/quare/bibleplanner/MainActivityViewModel.kt
@@ -1,10 +1,12 @@
 package com.quare.bibleplanner
 
 import androidx.lifecycle.ViewModel
+import com.quare.bibleplanner.core.model.NavigationEventBus
 import com.quare.bibleplanner.feature.materialyou.domain.usecase.GetIsDynamicColorsEnabledFlow
 
 class MainActivityViewModel(
     getIsDynamicColorsEnabledFlow: GetIsDynamicColorsEnabledFlow,
+    val navigationEventBus: NavigationEventBus,
 ) : ViewModel() {
     val isDynamicColorsEnabledFlow = getIsDynamicColorsEnabledFlow()
 }
