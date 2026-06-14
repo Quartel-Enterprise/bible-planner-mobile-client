d0b62371b63b51101bf63f8a9480be603c5a6754
Pierre Vieira <pierrevieiraggg@gmail.com>
2026-06-15 01:10:29 +0200

    fix: reset plan start date to today on logout (#157)

--- core/clear/build.gradle.kts
+++ core/clear/build.gradle.kts
@@ -13,6 +13,7 @@ kotlin {
         commonMain.dependencies {
             // Core
             implementation(projects.core.books)
+            implementation(projects.core.plan)
             implementation(projects.core.sync)
 
             // Coroutines
