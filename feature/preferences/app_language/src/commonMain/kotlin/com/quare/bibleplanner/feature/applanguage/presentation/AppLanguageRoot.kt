16cd8946a083998e0f4364331ef1e01f9bd63532
PierreVieira <pierrevieiraggg@gmail.com>
2026-07-09 11:17:12 -0300

    enhancement: show the language sheet as a dialog on wide layouts

--- feature/preferences/app_language/src/commonMain/kotlin/com/quare/bibleplanner/feature/applanguage/presentation/AppLanguageRoot.kt
+++ feature/preferences/app_language/src/commonMain/kotlin/com/quare/bibleplanner/feature/applanguage/presentation/AppLanguageRoot.kt
@@ -3,22 +3,25 @@ package com.quare.bibleplanner.feature.applanguage.presentation
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.dialog
 import com.quare.bibleplanner.core.model.route.AppLanguageNavRoute
 import com.quare.bibleplanner.feature.applanguage.presentation.model.AppLanguageUiEvent
 import com.quare.bibleplanner.feature.applanguage.presentation.utils.AppLanguageActionCollector
+import com.quare.bibleplanner.ui.component.ResponsiveDialogSheet
 import org.koin.compose.viewmodel.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 fun NavGraphBuilder.appLanguage(navController: NavHostController) {
-    dialog<AppLanguageNavRoute> {
+    dialog<AppLanguageNavRoute>(
+        dialogProperties = DialogProperties(usePlatformDefaultWidth = false),
+    ) {
         val viewModel = koinViewModel<AppLanguageViewModel>()
         val uiState by viewModel.uiState.collectAsState()
         AppLanguageActionCollector(
@@ -26,7 +29,8 @@ fun NavGraphBuilder.appLanguage(navController: NavHostController) {
             navController = navController,
         )
         val onEvent = viewModel::onEvent
-        ModalBottomSheet(onDismissRequest = { onEvent(AppLanguageUiEvent.OnDismiss) }) {
+
+        ResponsiveDialogSheet(onCloseClick = { onEvent(AppLanguageUiEvent.OnDismiss) }) {
             AppLanguageContent(
                 modifier = Modifier
                     .padding(horizontal = 16.dp)
