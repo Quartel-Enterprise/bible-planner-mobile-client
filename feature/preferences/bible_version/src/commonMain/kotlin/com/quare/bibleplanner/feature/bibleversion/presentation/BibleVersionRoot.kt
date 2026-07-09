7620760baf4523261ad07d9bd767034550779aad
Pierre Vieira <pierrevieiraggg@gmail.com>
2026-07-09 11:32:43 -0300

    enhancement: redesign the Bible version bottom sheet on the profile screen (#243)

--- feature/preferences/bible_version/src/commonMain/kotlin/com/quare/bibleplanner/feature/bibleversion/presentation/BibleVersionRoot.kt
+++ feature/preferences/bible_version/src/commonMain/kotlin/com/quare/bibleplanner/feature/bibleversion/presentation/BibleVersionRoot.kt
@@ -1,9 +1,9 @@
 package com.quare.bibleplanner.feature.bibleversion.presentation
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.collectAsState
@@ -12,6 +12,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.dialog
@@ -19,11 +20,14 @@ import com.quare.bibleplanner.core.model.route.BibleVersionSelectorRoute
 import com.quare.bibleplanner.feature.bibleversion.presentation.component.BibleVersionsContent
 import com.quare.bibleplanner.feature.bibleversion.presentation.model.BibleVersionUiEvent
 import com.quare.bibleplanner.feature.bibleversion.presentation.utils.BibleVersionsActionCollector
+import com.quare.bibleplanner.ui.component.ResponsiveDialogSheet
 import org.koin.compose.viewmodel.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 fun NavGraphBuilder.bibleVersionSelectionRoot(navController: NavHostController) {
-    dialog<BibleVersionSelectorRoute> {
+    dialog<BibleVersionSelectorRoute>(
+        dialogProperties = DialogProperties(usePlatformDefaultWidth = false),
+    ) {
         val viewModel: BibleVersionViewModel = koinViewModel()
         val onEvent = viewModel::onEvent
         val uiState by viewModel.uiState.collectAsState()
@@ -33,10 +37,13 @@ fun NavGraphBuilder.bibleVersionSelectionRoot(navController: NavHostController) 
             uiActionFlow = viewModel.uiAction,
             snackbarHostState = snackbarHostState,
         )
-        ModalBottomSheet(onDismissRequest = { onEvent(BibleVersionUiEvent.OnDismiss) }) {
+        ResponsiveDialogSheet(onCloseClick = { onEvent(BibleVersionUiEvent.OnDismiss) }) {
             Box {
                 BibleVersionsContent(
-                    modifier = Modifier.padding(bottom = 32.dp).padding(horizontal = 16.dp),
+                    modifier = Modifier
+                        .padding(bottom = 32.dp)
+                        .padding(horizontal = 16.dp)
+                        .navigationBarsPadding(),
                     uiState = uiState,
                     onEvent = onEvent,
                 )
